533519422f902b074e1942a6af146ef703e16aff
ahocevar <andreas.hocevar@gmail.com>
2013-10-10 08:28:23 +0200

    Simplifying example code

--- examples/districts.js
+++ examples/districts.js
@@ -9,23 +9,6 @@ var map = new ol.Map({
     source: new ol.source.Vector({
       url: 'http://data.wien.gv.at/daten/wfs?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json',
       parser: new ol.parser.GeoJSON()
-    }),
-    style: new ol.style.Style({
-      rules: [new ol.style.Rule({
-        symbolizers: [
-          new ol.style.Stroke({
-            width: 2,
-            opacity: 1
-          })
-        ]
-      }), new ol.style.Rule({
-        maxResolution: 20,
-        symbolizers: [
-          new ol.style.Text({
-            text: new ol.expr.parse('NAMEK')
-          })
-        ]
-      })]
     })
   })]
 });
