8fd8968351aa37ba8dbc78bc9877b5c1044b0aef
Andreas Hocevar <andreas.hocevar@gmail.com>
2014-11-07 14:42:59 +0100

    Updates

--- examples/districts.js
+++ examples/districts.js
@@ -1,18 +1,19 @@
 var map = new ol.Map({
   target: 'map',
-  renderer: ol.RendererHint.CANVAS,
-  view: new ol.View2D({
+  view: new ol.View({
     center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
     zoom: 11
   }),
   layers: [
     new ol.layer.Tile({
-      source: new ol.source.MapQuestOpenAerial()
+      source: new ol.source.MapQuest({layer: 'sat'})
     }),
     new ol.layer.Vector({
-      source: new ol.source.Vector({
-        url: 'http://data.wien.gv.at/daten/wfs?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json',
-        parser: new ol.parser.GeoJSON()
+      source: new ol.source.GeoJSON({
+        projection: 'EPSG:3857',
+        url: 'http://data.wien.gv.at/daten/geo?service=WFS&version=1.1.0&' +
+            'request=GetFeature&typeName=ogdwien:BEZIRKSGRENZEOGD&' +
+            'srsName=EPSG:4326&outputFormat=json'
       })
     })
   ]
