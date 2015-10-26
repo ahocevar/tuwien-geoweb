75371ca7afc2141ab4f6d1b0ba96f1677e0d9460
Andreas Hocevar <andreas.hocevar@gmail.com>
2015-10-26 20:58:01 +0100

    Updates

--- examples/districts-with-style.js
+++ examples/districts-with-style.js
@@ -1,13 +1,13 @@
 var map = new ol.Map({
   target: 'map',
   view: new ol.View({
-    center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
+    center: ol.proj.fromLonLat([16.37, 48.21]),
     zoom: 11
   }),
   layers: [
     new ol.layer.Vector({
-      source: new ol.source.GeoJSON({
-        projection: 'EPSG:3857',
+      source: new ol.source.Vector({
+        format: new ol.format.GeoJSON(),
         url: 'http://data.wien.gv.at/daten/geo?service=WFS&version=1.1.0&' +
           'request=GetFeature&typeName=ogdwien:BEZIRKSGRENZEOGD&' +
           'srsName=EPSG:4326&outputFormat=json'
@@ -34,7 +34,6 @@ var map = new ol.Map({
         });
         var styles = [style];
         return function(feature, resolution) {
-          console.log(resolution)
           style.getText().setText(
               resolution < 20 ? feature.get('NAMEK_NUM') : '');
           return styles;
